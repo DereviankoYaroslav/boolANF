f8b3ca3299df942d28350f990e59bab8373a2b9d
olenaderevianko <dereviankoolenav@gmail.com>
2021-08-02 11:58:29 +0300

    +minimal degree

--- main.c
+++ main.c
@@ -107,6 +107,8 @@ void cyclicStructure(const int *sbox, int size);
 
 int fixedPoints(int *sbox, int size);
 
+int minDegree(int *sbox, int size, int count);
+
 int main(int args, char **argv) {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
@@ -518,6 +520,12 @@ int main(int args, char **argv) {
 
     int fp = fixedPoints(ar2,size);
     printf("\n");
+
+    int md = minDegree(ar2,size,n);
+
+    printf("DEG = %d ", md);
+    printf("\n");
+
 
     //int sbl[] = {3, 106, 87, 164, 169, 243, 112, 241, 109, 0, 128, 135, 90, 16, 129, 44, 28, 34, 157, 103, 35, 113, 143, 67, 172, 33, 210, 104, 24, 222, 152, 65, 23, 105, 51, 195, 204, 160, 74, 224, 179, 239, 218, 215, 197, 85, 56, 41, 27, 29, 198, 99, 186, 141, 155, 47, 140, 124, 170, 13, 206, 6, 177, 173, 146, 154, 214, 184, 187, 192, 227, 50, 255, 194, 233, 45, 188, 232, 9, 95, 11, 249, 223, 54, 14, 156, 237, 61, 55, 202, 166, 117, 70, 163, 121, 134, 15, 231, 151, 165, 250, 81, 211, 216, 228, 48, 196, 238, 84, 150, 46, 226, 101, 144, 108, 58, 64, 251, 37, 149, 183, 40, 252, 73, 102, 174, 52, 8, 208, 77, 212, 167, 242, 10, 229, 92, 100, 230, 98, 12, 136, 1, 59, 225, 161, 116, 69, 178, 219, 107, 153, 86, 71, 142, 115, 246, 125, 213, 190, 57, 7, 66, 133, 32, 118, 94, 162, 122, 30, 88, 76, 148, 20, 247, 39, 205, 203, 79, 145, 130, 83, 217, 31, 193, 221, 180, 2, 138, 191, 89, 137, 175, 158, 60, 17, 139, 201, 234, 240, 176, 4, 126, 49, 5, 235, 38, 110, 80, 119, 68, 120, 199, 131, 236, 220, 159, 253, 254, 53, 26, 93, 97, 245, 244, 75, 18, 209, 82, 207, 248, 63, 147, 185, 171, 132, 78, 43, 189, 200, 91, 22, 96, 181, 21, 36, 111, 72, 114, 19, 123, 25, 168, 42, 62, 127, 182};
 
@@ -2159,6 +2167,14 @@ int fixedPoints(int *sbox, int size){
         printf("\nFixed_points ain't found\n");
 
     return counter;
+}
+
+int minDegree(int *sbox, int size, int count){
+    int *ar = SBoxToBooleanFunc(sbox,size,count);
+    int *ar2 = linearCombinations(ar,size,count);
+    int *ar3 = propertiesOfLinearCombinations(ar2,size,count);
+    int result = ar3[2];
+    return result;
 }
 
 int testNL(int *sbox, int size, int count){
